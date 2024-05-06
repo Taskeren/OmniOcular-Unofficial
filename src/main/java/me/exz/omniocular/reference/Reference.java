f95e46425fcd9b180af0fe063e9fe2dfd0082b53
初夏同学 <2411829240@qq.com>
2024-05-06 22:11:58 +0800

    Fix version tag

--- src/main/java/me/exz/omniocular/reference/Reference.java
+++ src/main/java/me/exz/omniocular/reference/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
 
     public static final String MOD_ID = "OmniOcular";
     public static final String MOD_NAME = "Omni Ocular";
-    public static final String VERSION = "VERSION";
+    public static final String VERSION = Tags.VERSION;
     public static final String CLIENT_PROXY_CLASS = "me.exz.omniocular.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "me.exz.omniocular.proxy.ServerProxy";
 }
