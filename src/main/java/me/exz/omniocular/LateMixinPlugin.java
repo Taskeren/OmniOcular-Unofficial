d5dcf8a8555f16be58463ad2de47cdec36bc723c
初夏同学 <2411829240@qq.com>
2024-06-12 22:26:26 +0800

    spotless

--- src/main/java/me/exz/omniocular/LateMixinPlugin.java
+++ src/main/java/me/exz/omniocular/LateMixinPlugin.java
@@ -1,14 +1,15 @@
 package me.exz.omniocular;
 
-import com.gtnewhorizon.gtnhmixins.ILateMixinLoader;
-import com.gtnewhorizon.gtnhmixins.LateMixin;
-
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
+import com.gtnewhorizon.gtnhmixins.ILateMixinLoader;
+import com.gtnewhorizon.gtnhmixins.LateMixin;
+
 @LateMixin
 public class LateMixinPlugin implements ILateMixinLoader {
+
     @Override
     public String getMixinConfig() {
         return "mixins.OmniOcularUnofficial.late.json";
